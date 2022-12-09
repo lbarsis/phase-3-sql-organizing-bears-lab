d918bc86845987f2ba277280da5fbf1c5009aa1f
Levi Barsis <lbarsis@gmail.com>
2022-12-08 20:33:45 -0800

    Done with assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 17, "F", "brown", "bear", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 5, "M", "black", "bear", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 14, "M", "brown", "bear", 1);
+INSERT INTO bears VALUES (4,"Sargeant Brown", 26, "M", "bear", "Slimy", 1);
+INSERT INTO bears VALUES (5,"Melissa", 47, "F", "brown", "bear", 1);
+INSERT INTO bears VALUES (6,"Grinch", 55, "F", "black", "bear", 1);
+INSERT INTO bears VALUES (7,"Wendy", 2, "F", "brown", "bear", 1);
+INSERT INTO bears VALUES (8,null, 19, "F", "black", "bear", 1);
